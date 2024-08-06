e0d2d3c15559a1525dc55fe725b870b961edb4eb
ZarciaM <154671918+ZarciaM@users.noreply.github.com>
2024-08-06 14:20:01 +0200

    feat: react PROPS, PROPTYPES and DDEFAULTPROPS

--- my-react-app1/src/App.jsx
+++ my-react-app1/src/App.jsx
@@ -1,15 +1,25 @@
-import Card from './Card.jsx'
+//HOW TO STYLE REACT COMPONENTS WITH CSS
+//.....................................
+// (not including external framework or preprocessor)
+
+// 1. EXTERNAL
+// 2. MODULES
+// 3. INLINE
+
+import Student from './Student.jsx'
 
 
 function App() {
   return(
-     
     <>
-    <Card/>
-    <Card/>
-    <Card/>
+    <Student name = "SpongeBobb" age= {3} isStudent = {true}/>
+    <Student name = "Patric" age= {40} isStudent = {false}/>
+    <Student name = "Sandy" age= {20} isStudent = {true}/>
+    <Student age = {15}/>
     </>
+
   );
 }
+
 
 export default App
---
+++ my-react-app1/src/Button.jsx
@@ -0,0 +1,6 @@
+
+    function Button(){
+        return <button className="button">Click me</button>
+    }
+
+    export default Button
